ba623e7d70446d66e7da685fc28119a8e89ade39
Megafu <megafu.charles@yahoo.com>
2019-02-22 15:43:20 +0100

    Started the test applcation

---
+++ src/main/java/com/heycar/test/HeyCarTestApplication.java
@@ -0,0 +1,13 @@
+package com.heycar.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyCarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyCarTestApplication.class, args);
+	}
+
+}
